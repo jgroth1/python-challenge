425a21711ad0df3c50233adf5974d2bedcbdb310
Jonathan Groth <jon.groth1@gmail.com>
2018-05-21 17:16:50 -0400

    finished PyBank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -38,9 +38,34 @@ with open(file_path, newline='') as bankfile:
     # loop through the rows of the bank data
     for row in bankdata:
         # append values to revenue values to revenue list, month and date to month_date list
-        revenue.append(row[1])
+        revenue.append(float(row[1]))
         month_date.append(row[0])
         # split month date by '-' to obtain only the month. append the month only to month list 
         temp = row[0].split('-')
         month.append(temp[0])
 
+# determine the number of months by converting the list to a set that can only contain unique values then taking the length of the set using len
+month_set = set(month)
+num_months = len(month_set)
+
+# compute total revenue using the sum()
+total_revenue = round(sum(revenue), 2)
+
+# compute average revenue change by using mean() from the statistics package
+avg_revenue = round(mean(revenue), 2)
+
+
+# find the greatest increase in revenue using max() and find the index using the .index() method
+max_value = max(revenue)
+max_index = revenue.index(max_value)
+
+# find the greatest decrease in revenue using min() and find the index using the .index() method
+min_value = min(revenue)
+min_index = revenue.index(min_value)
+
+# find the month and date that coresponds to the greatest increase and decrease in revenue using the month_date list and index in max_index and min_index
+max_date = month_date[max_index]
+min_date = month_date[min_index]
+
+
+print('','Financial Analysis', '-----------------------', 'Total Months: ' + str(num_months), 'Total Revenue: $' + str(total_revenue), 'Average Revenue Change: $' + str(avg_revenue),'Greatest Increase in Revenue: ' + max_date + '($' + ' ' + str(max_value) + ')', 'Greatest Decrease in Revenue: ' + min_date + '($' + ' ' + str(min_value) + ')', '',sep='\n',)
