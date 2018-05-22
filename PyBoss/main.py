7b565580b68d7ca372cf592996f3bd36598529f2
Jonathan Groth <jon.groth1@gmail.com>
2018-05-22 01:58:14 -0400

    finished code for pyboss need to comment

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -25,13 +25,106 @@
 import os
 import csv
 
+# set up dictionary of state names to abriviations taken from https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 # set file path
 file = 'employee_data1.csv'
 file_path = os.path.join('../Data_Files/PyBoss/raw_data', file)
 
+# Open file with in the with operator and read using csv.reader
 with open(file_path, newline='') as data_file:
     employee_data = csv.reader(data_file, delimiter=',')
+    # skip header line
+    next(employee_data)
+    # list for id, first name, dob, ssn, and state 
+    ID = []
+    FirstName = []
+    LastName = []
+    DOB = []
+    SSN = []
+    State = []
 
-    new_employee_list = []
+    # Loop through the rows of empoyee_data
     for row in employee_data:
-        
+        ID.append(row[0])
+        temp = row[1].split(' ')
+        FirstName.append(temp[0])
+        LastName.append(temp[1])
+        temp = row[2].split('-')
+        temp1 = temp[1] + '/' + temp[2] + '/' + temp[0]
+        DOB.append(temp1)
+        temp = row[3].split('-')
+        temp1 = '***-**-' + temp[2]
+        SSN.append(temp1)
+        temp = us_state_abbrev[row[4]]
+        State.append(temp)
+
+header = ['ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State']
+new_employee_list = []
+for i in range(len(ID)):
+    new_employee_dict = {}
+    new_employee_dict[header[0]] = ID[i]
+    new_employee_dict[header[1]] = FirstName[i]
+    new_employee_dict[header[2]] = LastName[i]
+    new_employee_dict[header[3]] = DOB[i]
+    new_employee_dict[header[4]] = SSN[i]
+    new_employee_dict[header[5]] = State[i]
+    new_employee_list.append(new_employee_dict)
+
+with open('new_employee_data.csv', 'w') as datafile:
+    writer = csv.DictWriter(datafile, fieldnames=header)
+
+    writer.writeheader()
+    writer.writerows(new_employee_list)
